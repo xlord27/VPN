3c59c6df62185a7778523bb51a652c8264eb00ed
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-06 05:44:08 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -102,7 +102,7 @@ echo -e "$green      Install Websocket              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/xlord27/VPN/main/Insshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/xlord27/gabut/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 
 #exp
 cd /usr/bin
